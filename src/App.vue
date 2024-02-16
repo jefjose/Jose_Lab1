de656583ccddacce90dd5da497ab847664d741e5
jefjose <joshuaemmanuel.jose.cics@ust.edu.ph>
2024-02-16 20:45:27 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,131 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div id="app">
+    <h1>Rock Paper Scissors</h1>
+    <div>
+      <img src="/rock.jpg" alt="Rock" class="choice-img" @click="play('rock')">
+      <img src="/paper.jpg" alt="Paper" class="choice-img" @click="play('paper')">
+      <img src="/scissors.jpg" alt="Scissors" class="choice-img" @click="play('scissors')">
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+      <h3>VS</h3>
+    </div>
+    <div>
+      <img :src="chosenImage" alt="Computer Choice" class="choice-img">
+    </div>
+    <div class="result">
+      <p v-if="result !== null">{{ result }}</p>
+      <p>Wins: {{ wins }}</p>
+      <p>Ties: {{ ties }}</p>
+      <p>Losses: {{ losses }}</p>
+      <button @click="reset">Reset</button>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      game: new Game(),
+      chosenImage: '/placeholder.jpg'
+    };
+  },
+  methods: {
+    play(choice) {
+      this.game.play(choice);
+      if (this.game.computerChoice !== null) {
+        this.chosenImage = '/' + this.game.computerChoice + '.jpg';
+      }
+    },
+    reset() {
+      this.game.reset();
+      this.chosenImage = '/placeholder.jpg';
+    }
+  },
+  
+  computed: {
+    result() {
+      return this.game.result;
+    },
+    wins() {
+      return this.game.wins;
+    },
+    ties() {
+      return this.game.ties;
+    },
+    losses() {
+      return this.game.losses;
+    }
+  }
+};
+
+class Game {
+  constructor() {
+    this.choices = ['rock', 'paper', 'scissors'];
+    this.result = "Choose your pick!";
+    this.computerChoice = null;
+    this.wins = 0;
+    this.ties = 0;
+    this.losses = 0;
+  }
+
+  play(userChoice) {
+    const computerChoice = this.choices[Math.floor(Math.random() * this.choices.length)];
+    this.computerChoice = computerChoice;
+
+    if (userChoice === computerChoice) {
+      this.result = "It's a tie!";
+      this.ties++;
+    } else if (
+      (userChoice === 'rock' && computerChoice === 'scissors') ||
+      (userChoice === 'paper' && computerChoice === 'rock') ||
+      (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      this.result = 'You win!';
+      this.wins++;
+    } else {
+      this.result = 'Computer wins!';
+      this.losses++;
+    }
+  }
+
+  reset() {
+    this.result = "Choose your pick!";
+    this.computerChoice = null;
+    this.wins = 0;
+    this.ties = 0;
+    this.losses = 0;
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+body {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#app {
+  text-align: center;
+  position: fixed;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.choice-img {
+  width: 100px;
+  cursor: pointer;
+  margin: 5px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.result {
+  position: fixed;
+  top: 450px;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
